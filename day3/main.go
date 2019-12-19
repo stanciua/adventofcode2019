e2381047dd878569d861702124c58026fd2c5c94
stanciua <andreistanciu.as@gmail.com>
2019-12-19 15:30:11 +0200

    Day 3 part 1 ongoing

--- day3/main.go
+++ day3/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -42,6 +43,8 @@ func main() {
 	// build the initial grid
 	grid := buildInitialGrid(initialGridSize)
 
+	// plot the path for wire 1
+	plotPath(grid, wirePath1)
 	displayGrid(grid)
 	// part 1 solution
 	fmt.Println("The result to 1st part is: ", part1(grid))
@@ -58,17 +61,17 @@ func part2(grid [][]rune) int {
 	return -1
 }
 
-type direction int
+type Direction int
 
 const (
-	up direction = iota
+	up Direction = iota
 	down
 	left
 	right
 )
 
-func directionFromString(s string) direction {
-	var d direction
+func directionFromString(s string) Direction {
+	var d Direction
 
 	switch s {
 	case "U":
@@ -164,12 +167,45 @@ func resizeGrid(grid [][]rune) [][]rune {
 
 func plotPath(grid [][]rune, path []string) {
 	x0, y0 := getGridOrigin(grid)
-	lastStep := '-'
+	// get the direction for the first step
+	direction := directionFromString(path[0][:1])
+	lastStep := getSymbolForDirection(direction)
 	grid[x0][y0] = 'o'
+	currX, currY := x0, y0
 	for _, step := range path {
 		direction := directionFromString(step[:1])
+		symbol := getSymbolForDirection(direction)
+		if lastStep != symbol {
+			grid[currX][currY] = '+'
+		}
+		offsetX := 0
+		offsetY := 0
+		switch direction {
+		case up:
+			offsetX = -1
+		case down:
+			offsetX = 1
+		case left:
+			offsetY = -1
+		case right:
+			offsetY = 1
+		}
 		if noOfSteps, err := strconv.Atoi(step[1:]); err == nil {
-
+			for i := 0; i < noOfSteps; i++ {
+				// we need to check if we can move in that direction, if not
+				// resize the grid
+				if currX+offsetX >= len(grid) ||
+					currY+offsetY >= len(grid[0]) {
+					originOffsetX, originOffsetY := int(math.Abs(float64(currX)-float64(x0))), int(math.Abs(float64(currY)-float64(y0)))
+					grid = resizeGrid(grid)
+					origX0, origY0 := getGridOrigin(grid)
+					currX, currY = origX0+originOffsetX, origY0+originOffsetY
+				}
+				currX += offsetX
+				currY += offsetY
+				grid[currX][currY] = symbol
+				lastStep = symbol
+			}
 		} else {
 			panic(err)
 		}

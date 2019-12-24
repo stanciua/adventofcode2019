f20cc53f482506cb2673a6f75873dfd456e0b3ab
stanciua <andreistanciu.as@gmail.com>
2019-12-24 23:09:19 +0200

    Day 4: part 2 ongoing

--- day4/main.go
+++ day4/main.go
@@ -42,8 +42,6 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(findAdjacentDigitsPositions(3, []int{1, 1, 1, 4, 5, 6}))
-
 	// part 1 solution
 	fmt.Println("The result to 1st part is: ", part1(rmin, rmax))
 	// part 2 solution
@@ -80,7 +78,7 @@ func part1(rmin, rmax int) int {
 	noOfPasswords := 0
 	for i := rmin; i <= rmax; i++ {
 		digits := getNumberDigits(i)
-		if twoAdjacentDigitsAreTheSame(digits) && doDigitsIncreaseOrStayTheSame(digits) {
+		if _, present := twoAdjacentDigitsStartingPos(digits); present && doDigitsIncreaseOrStayTheSame(digits) {
 			noOfPasswords++
 		}
 	}
@@ -88,54 +86,87 @@ func part1(rmin, rmax int) int {
 	return noOfPasswords
 }
 
-// we need to bypass the following combinations
-// aaaaaa
-// aaaaab
-// baaaaa
-// aaaabc, b != c
-// bcaaaa, b != c
-// aaabcd, b != c, c != d
-// baaacd, c != d
-// bcaaad, b != c
-// bcdaaa, b != c, c != d
-
 func part2(rmin, rmax int) int {
 	noOfPasswords := 0
-	noOfPasswords++
-	// 111122
 	for i := rmin; i <= rmax; i++ {
 		digits := getNumberDigits(i)
-
-		if !twoAdjacentDigitsAreTheSame(digits) || !doDigitsIncreaseOrStayTheSame(digits) {
+		if i == 136888 {
+			fmt.Println(i)
+		}
+		startPos, present := twoAdjacentDigitsStartingPos(digits)
+		if !present || !doDigitsIncreaseOrStayTheSame(digits) {
 			continue
 		}
 
-		if (digits[0] == digits[1] && digits[1] == digits[2] && digits[2] == digits[3] && digits[3] == digits[4] && digits[4] == digits[5]) ||
-			(digits[0] == digits[1] && digits[1] == digits[2] && digits[2] == digits[3] && digits[3] == digits[4]) ||
-			(digits[1] == digits[2] && digits[2] == digits[3] && digits[3] == digits[4] && digits[4] == digits[5]) ||
-			(digits[0] == digits[1] && digits[1] == digits[2] && digits[2] == digits[3] && digits[4] != digits[5]) ||
-			(digits[0] != digits[1] && digits[2] == digits[3] && digits[3] == digits[4] && digits[4] == digits[5]) ||
-			(digits[0] == digits[1] && digits[1] == digits[2] && digits[3] != digits[4] && digits[4] && digits[5]) ||
-			(digits[1] == digits[2] && digits[2] == digits[3] && digits[4] != digits[5]) ||
-			(digits[0] != digits[1] && digits[2] == digits[3] && digits[3] == digits[4]) ||
-			(digits[0] != digits[1] && digits[1] != digits[2] && digits[2] == digits[3] && digits[3] == digits[4] && digits[4] == digits[5]) {
-			continue
+		pos, groupLen, found := moreThenTwoAdjacentDigitsPosition(digits, startPos)
+
+		// check the left side and right side
+		if found {
+			foundLeft := checkSubsliceForTwoAdjacentDigits(digits[0:pos])
+			foundRight := checkSubsliceForTwoAdjacentDigits(digits[startPos+groupLen : len(digits)])
+			if foundLeft || foundRight {
+				noOfPasswords++
+			}
 		}
+
 	}
 	return noOfPasswords
 }
 
-func findAdjacentDigitsPositions(noOfAdjacentDigits int, digits []int) []int {
-	var positions []int
-	for i, j := 0, 1; i < len(digits) && j < len(digits)-1; i, j = i+1, j+1 {
-		if digits[i] == digits[j] {
-			positions = append(positions, i)
+func twoAdjacentDigitsStartingPos(digits []int) (int, bool) {
+	if digits[0] == digits[1] {
+		return 0, true
+	} else if digits[1] == digits[2] {
+		return 1, true
+	} else if digits[2] == digits[3] {
+		return 2, true
+	} else if digits[3] == digits[4] {
+		return 3, true
+	} else if digits[4] == digits[5] {
+		return 4, true
+	}
+
+	return -1, false
+}
+
+func moreThenTwoAdjacentDigitsPosition(digits []int, startPos int) (int, int, bool) {
+	// look on the left side
+	newStartPos := 0
+	lenAdjacent := 2
+	foundMoreThanTwoDigits := false
+	for i := startPos - 1; i >= 0; i-- {
+		if digits[startPos] == digits[i] {
+			newStartPos = i
+			lenAdjacent++
+			foundMoreThanTwoDigits = true
+		}
+	}
+	// look on the right side
+	for i := startPos + 2; i < len(digits); i++ {
+		if digits[startPos] == digits[i] {
+			if !foundMoreThanTwoDigits {
+				lenAdjacent++
+				newStartPos = startPos
+			} else {
+				lenAdjacent++
+			}
 		}
 	}
 
-	if len(positions) != noOfAdjacentDigits {
-		return nil
-	}
+	return newStartPos, lenAdjacent, foundMoreThanTwoDigits
+}
 
-	return positions
+func checkSubsliceForTwoAdjacentDigits(digits []int) bool {
+	count := 0
+	if len(digits) >= 2 {
+		i := 0
+		j := i + 1
+		for i < len(digits) && j < len(digits)-1 {
+			if digits[i] == digits[j] {
+				count++
+			}
+		}
+
+	}
+	return count == 1
 }

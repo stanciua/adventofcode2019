a49dc72a23014c97ba3d8923140fe07e534c8e3f
Andrei Stanciu <stanciua@Andreis-MacBook-Pro.local>
2022-09-06 22:55:53 +0300

    samples input work correctly, but puzzle input takes too long, requires optimations...

--- day18/main.go
+++ day18/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
-	"fmt"
-	// pp "github.com/k0kubun/pp/v3"
+	// "fmt"
+	"github.com/k0kubun/pp"
 	"log"
 	"math"
 	"os"
@@ -38,9 +38,10 @@ func findEntrance(m [][]rune) Vertex {
 	return Vertex{-1, -1}
 }
 
-func isAllowed(s rune) bool {
+func isAllowed(s rune, visitedKeys map[rune]bool) bool {
 	if s == OpenPassage ||
-		unicode.IsLower(s) {
+		unicode.IsLower(s) ||
+		(unicode.IsUpper(s) && visitedKeys[unicode.ToLower(s)]) {
 		return true
 	}
 	return false
@@ -57,22 +58,22 @@ func adjacentVertices(v Vertex, edges []Edge) []Vertex {
 	}
 	return vertices
 }
-func findNeighbors(p Vertex, m map[Vertex]rune) []Vertex {
+func findNeighbors(p Vertex, m map[Vertex]rune, visitedKeys map[rune]bool) []Vertex {
 	neighbors := make([]Vertex, 0)
 	pl := Vertex{p.y - 1, p.x}
-	if _, ok := m[pl]; ok && isAllowed(m[pl]) {
+	if _, ok := m[pl]; ok && isAllowed(m[pl], visitedKeys) {
 		neighbors = append(neighbors, pl)
 	}
 	pl = Vertex{p.y + 1, p.x}
-	if _, ok := m[pl]; ok && isAllowed(m[pl]) {
+	if _, ok := m[pl]; ok && isAllowed(m[pl], visitedKeys) {
 		neighbors = append(neighbors, pl)
 	}
 	pl = Vertex{p.y, p.x - 1}
-	if _, ok := m[pl]; ok && isAllowed(m[pl]) {
+	if _, ok := m[pl]; ok && isAllowed(m[pl], visitedKeys) {
 		neighbors = append(neighbors, pl)
 	}
 	pl = Vertex{p.y, p.x + 1}
-	if _, ok := m[pl]; ok && isAllowed(m[pl]) {
+	if _, ok := m[pl]; ok && isAllowed(m[pl], visitedKeys) {
 		neighbors = append(neighbors, pl)
 	}
 
@@ -89,131 +90,94 @@ func findIndex(Q []Vertex, p Vertex) (bool, int) {
 	return false, -1
 }
 
-// procedure DFS_iterative(G, v) is
-//
-//	let S be a stack
-//	S.push(v)
-//	while S is not empty do
-//	    v = S.pop()
-//	    if v is not labeled as discovered then
-//	        label v as discovered
-//	        for all edges from v to w in G.adjacentEdges(v) do
-//	            S.push(w)
-// func dfs(vertices []Vertex, edges []Edge, graph map[Edge]int, source Vertex, keys map[rune]Vertex) int {
-// 	min := math.MaxInt
-// 	// paths := make([][]string, 0)
-//
-// 	S := make([]Vertex, 0)
-// 	discovered := make(map[Vertex]bool)
-// 	S = append(S, source)
-// 	for len(S) > 0 {
-// 		v := S[len(S)-1]
-// 		S = S[:len(S)-1]
-//
-// 		fmt.Println(S)
-// 		if isValidPath(S, vertices) {
-// 			fmt.Println(S)
-// 			if steps := steps(S, graph); steps < min {
-// 				min = steps
-// 			}
-// 		}
-//
-// 		if !discovered[v] {
-// 			discovered[v] = true
-// 			for _, w := range adjacentVertices(v, edges) {
-// 				S = append(S, w)
-// 			}
-// 		}
-// 	}
-//
-// 	fmt.Println(graph)
-// 	return min
-// }
-
-// func steps(path []Vertex, graph map[Edge]int) int {
-// 	sum := 0
-// 	for i := 0; i < len(path)-1; i++ {
-// 		s := graph[Edge{path[i], path[i+1]}] + graph[Edge{path[i+1], path[i]}]
-// 		fmt.Println(s)
-// 		sum += s
-// 	}
-// 	return sum
-// }
-//
-// func isValidPath(path []Vertex, sourcePoints []Vertex) bool {
-// 	// if len(path) != len(sourcePoints) {
-// 	// 	return false
-// 	// }
-// 	ms := make(map[Vertex]bool)
-// 	for _, v := range sourcePoints {
-// 		ms[v] = true
-// 	}
-//
-// 	mp := make(map[Vertex]bool)
-// 	for _, v := range path {
-// 		mp[v] = true
-// 	}
-//
-// 	for k := range ms {
-// 		if !mp[k] {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
-
-func dijkstraRedo(vertices []Vertex, edges []Edge, graph map[Edge]int, source Vertex, keys map[rune]Vertex) [][]string {
-	dist := make(map[Vertex]int)
-	prev := make(map[Vertex]Vertex)
+func isKeyReachable(graph map[Vertex]rune, source Vertex, destination Vertex, visitedKeys map[rune]bool) bool {
 	Q := make([]Vertex, 0)
-	// pp.Println(graph)
-	for _, k := range vertices {
-		dist[k] = math.MaxInt
-		Q = append(Q, k)
-	}
+	explored := make(map[Vertex]bool)
+	explored[source] = true
+	Q = append(Q, source)
 
-	dist[source] = 0
 	for len(Q) > 0 {
-		u := findMinDistance(Q, dist)
-		if found, i := findIndex(Q, u); found {
-			Q = append(Q[:i], Q[i+1:]...)
+		v := Q[0]
+		Q = Q[1:]
+		if v == destination {
+			return true
 		}
 
-		for _, v := range adjacentVertices(u, edges) {
-			if found, _ := findIndex(Q, v); found {
-				steps := 0
-				if s1, ok := graph[Edge{u, v}]; ok {
-					steps = s1
-				} else if s2, ok := graph[Edge{v, u}]; ok {
-					steps = s2
-				}
-				alt := dist[u] + steps
-				if alt < dist[v] {
-					dist[v] = alt
-					prev[v] = u
-				}
+		for _, w := range findNeighbors(v, graph, visitedKeys) {
+			if !explored[w] {
+				explored[w] = true
+				Q = append(Q, w)
 			}
 		}
 	}
 
-	pkey := make(map[Vertex]rune)
-	for k1, v := range keys {
-		pkey[v] = k1
-	}
-	// reconstruct all the paths
-	paths := make([][]string, 0)
-	for _, u := range vertices[1:] {
-		S := make([]string, 0)
-		if _, ok := prev[u]; ok || u == source {
-			for _, ok := prev[u]; ok; u, ok = prev[u] {
-				S = append([]string{string(pkey[u])}, S...)
-			}
+	return false
+}
+
+func reachableKeys(key Vertex, keys []Vertex, graph map[Vertex]rune, visitedKeys map[rune]bool) []Vertex {
+	reachable := make([]Vertex, 0)
+
+	for _, v := range keys {
+		if isKeyReachable(graph, key, v, visitedKeys) {
+			reachable = append(reachable, v)
 		}
-		paths = append(paths, S)
+	}
+	return reachable
+}
+
+type CacheKey struct {
+	key           Vertex
+	remainingKeys [26]Vertex
+}
+
+func minDistance(graph map[Vertex]rune, distances map[Edge]int, currentKey Vertex, remainingKeys *[]Vertex, visitedKeys map[rune]bool, cache map[CacheKey]int) int {
+	if len(*remainingKeys) == 0 {
+		return 0
 	}
 
-	// pp.Println(paths)
-	return paths
+	var remainingKeyCache [26]Vertex
+	copy(remainingKeyCache[:], *remainingKeys)
+	cacheKey := CacheKey{currentKey, remainingKeyCache}
+
+	if d, ok := cache[cacheKey]; ok {
+		return d
+	}
+
+	min := math.MaxInt
+
+	reachableKeys := reachableKeys(currentKey, *remainingKeys, graph, visitedKeys)
+	for _, k := range reachableKeys {
+		*remainingKeys = removeKey(*remainingKeys, k)
+		visitedKeys[graph[k]] = true
+		distance := 0
+		if d, ok := distances[Edge{currentKey, k}]; ok {
+			distance = d
+		} else {
+			pp.Println(string(graph[currentKey]), string(graph[k]))
+		}
+		distance += minDistance(graph, distances, k, remainingKeys, visitedKeys, cache)
+		delete(visitedKeys, graph[k])
+		*remainingKeys = append(*remainingKeys, k)
+
+		if distance < min {
+			min = distance
+		}
+	}
+
+	copy(remainingKeyCache[:], *remainingKeys)
+	cacheKey = CacheKey{currentKey, remainingKeyCache}
+	cache[cacheKey] = min
+	return min
+}
+
+func removeKey(keys []Vertex, key Vertex) []Vertex {
+	for i, k := range keys {
+		if key == k {
+			return append(keys[:i], keys[i+1:]...)
+		}
+	}
+
+	return keys
 }
 
 func dijkstra(m map[Vertex]rune, source Vertex, destination Vertex, k map[rune]Vertex, doors map[rune]Vertex) int {
@@ -231,6 +195,7 @@ func dijkstra(m map[Vertex]rune, source Vertex, destination Vertex, k map[rune]V
 	}
 
 	dist[source] = 0
+	visitedKeys := make(map[rune]bool)
 	for len(Q) > 0 {
 		u := findMinDistance(Q, dist)
 		if u == destination {
@@ -249,7 +214,7 @@ func dijkstra(m map[Vertex]rune, source Vertex, destination Vertex, k map[rune]V
 			}
 		}
 
-		for _, v := range findNeighbors(u, m) {
+		for _, v := range findNeighbors(u, m, visitedKeys) {
 			if found, _ := findIndex(Q, v); found {
 				alt := dist[u] + 1
 				if alt < dist[v] {
@@ -281,7 +246,8 @@ func findKeysAndDoors(m [][]rune) (map[rune]Vertex, map[rune]Vertex) {
 		for j, s := range line {
 			if unicode.IsUpper(s) {
 				doors[s] = Vertex{i, j}
-			} else if unicode.IsLower(s) || s == Entrance {
+				// } else if unicode.IsLower(s) || s == Entrance {
+			} else if unicode.IsLower(s) {
 				keys[s] = Vertex{i, j}
 			}
 		}
@@ -290,10 +256,29 @@ func findKeysAndDoors(m [][]rune) (map[rune]Vertex, map[rune]Vertex) {
 	return keys, doors
 }
 
+func preCalculateDistances(graph map[Vertex]rune, keysList []Vertex, keys map[rune]Vertex, doors map[rune]Vertex) map[Edge]int {
+	distances := make(map[Edge]int)
+	for i := 0; i < len(keysList); i++ {
+		for j := 0; j < len(keysList); j++ {
+			if i == j {
+				continue
+			}
+			d := dijkstra(graph, keysList[i], keysList[j], keys, doors)
+			key := Edge{keysList[i], keysList[j]}
+			if _, ok := distances[key]; ok {
+				continue
+			}
+			distances[key] = d
+		}
+	}
+
+	return distances
+}
+
 func part1(m [][]rune) int {
 	entrance := findEntrance(m)
-	keys, doors := findKeysAndDoors(m)
 	m[entrance.y][entrance.x] = OpenPassage
+	keys, doors := findKeysAndDoors(m)
 	sources := make([]rune, 0)
 	for k := range keys {
 		sources = append(sources, k)
@@ -303,57 +288,39 @@ func part1(m [][]rune) int {
 		return sources[i] < sources[j]
 	})
 	sourcePoints := make([]Vertex, 0)
+	// add the entrance also
 	sourcePoints = append(sourcePoints, entrance)
 	for _, s := range sources {
 		sourcePoints = append(sourcePoints, keys[s])
 	}
 
 	// build a map of all the locations except walls
-	minMap := make(map[Vertex]rune)
+	graph := make(map[Vertex]rune)
 	for i := range m {
 		for j := range m[i] {
 			if m[i][j] != Wall {
-				minMap[Vertex{i, j}] = m[i][j]
+				graph[Vertex{i, j}] = m[i][j]
 			}
 		}
 	}
 
-	graph := make(map[Edge]int)
-	edges := make([]Edge, 0)
-	for _, s1 := range sourcePoints {
-		for _, s2 := range sourcePoints {
-			if s1 == s2 {
-				continue
-			}
-			e := Edge{s1, s2}
-			ei := Edge{s2, s1}
-			_, ok1 := graph[e]
-			_, ok2 := graph[ei]
-			if ok1 || ok2 {
-				continue
-			}
-			edges = append(edges, Edge{s1, s2})
-			step := dijkstra(minMap, s1, s2, keys, doors)
-			graph[e] = step
+	// copy of the keys
+	remainingKeys := make([]Vertex, 0)
+	for _, k := range sources {
+		if k == Entrance {
+			continue
 		}
+		remainingKeys = append(remainingKeys, keys[k])
 	}
 
-	pkey := make(map[Vertex]rune)
-	for k1, v := range keys {
-		pkey[v] = k1
+	visitedKeys := make(map[rune]bool)
+	copyGraph := make(map[Vertex]rune)
+	for k, v := range graph {
+		copyGraph[k] = v
 	}
-	for _, e := range edges {
-		fmt.Println(string(pkey[e.v1]), " -> ", string(pkey[e.v2]))
-	}
-	// for _, s := range sources {
-	// 	paths := dijkstraRedo(sourcePoints, edges, graph, keys[s], keys)
-	// 	for _, p := range paths {
-	// 		pp.Println(p)
-	// 		pp.Println("----------------------------------------")
-	// 	}
-	// }
-
-	return -1
+	cache := make(map[CacheKey]int)
+	distances := preCalculateDistances(copyGraph, sourcePoints, keys, doors)
+	return minDistance(graph, distances, entrance, &remainingKeys, visitedKeys, cache)
 }
 
 func part2(m [][]rune) int {
@@ -388,8 +355,8 @@ func main() {
 		log.Fatal(err)
 	}
 	// part 1 solution
-	fmt.Println("The result to 1st part is: ", part1(m))
+	pp.Println("The result to 1st part is: ", part1(m))
 
 	// part 2 solution
-	fmt.Println("The result to 2nd part is: ", part2(m))
+	pp.Println("The result to 2nd part is: ", part2(m))
 }

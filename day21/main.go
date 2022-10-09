a9d973fca8d65eec98dbed945e61c962734d0894
Andrei Stanciu <stanciua@Andreis-MacBook-Pro.local>
2022-10-09 22:53:55 +0300

    Finished Day 21 part 1 and 2.

---
+++ day21/main.go
@@ -0,0 +1,324 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type SpringDroid struct {
+	vm *VM
+}
+
+type VM struct {
+	memory             []int64
+	currInstruction    *Instruction
+	input              []int64
+	instructionPointer int64
+	output             int64
+	relativeBase       int64
+	outputReady        bool
+	stopped            bool
+}
+
+type Instruction struct {
+	opcode    Opcode
+	paramMode []ParameterMode
+	params    []int64
+	length    int64
+}
+
+func NewVM() *VM {
+	return new(VM)
+}
+
+func (vm *VM) loadProgram(input []int64) {
+	vm.memory = append(vm.memory, input...)
+}
+
+type Opcode int64
+
+const (
+	Add Opcode = iota + 1
+	Multiply
+	Input
+	Output
+	JumpIfTrue
+	JumpIfFalse
+	LessThan
+	Equals
+	UpdateRelativeBase
+	And
+	Or
+	Not
+	ProgramStop = 99
+)
+
+type ParameterMode int64
+
+const (
+	Positional ParameterMode = iota
+	Immediate
+	Relative
+)
+
+func (vm *VM) hasFinished() bool {
+	return vm.stopped
+}
+
+func (vm *VM) decodeCurrentInstruction() *Instruction {
+	instruction := new(Instruction)
+	opcode := vm.load(vm.instructionPointer)
+	instruction.opcode = Opcode(opcode % 100)
+	switch Opcode(instruction.opcode) {
+	case Add, Multiply, LessThan, Equals:
+		instruction.params = []int64{vm.load(vm.instructionPointer + 1),
+			vm.load(vm.instructionPointer + 2),
+			vm.load(vm.instructionPointer + 3)}
+		instruction.paramMode = []ParameterMode{ParameterMode(opcode / 100 % 10),
+			ParameterMode(opcode / 1000 % 10),
+			ParameterMode(opcode / 10000 % 10)}
+		instruction.length = 4
+	case Input, Output, UpdateRelativeBase:
+		instruction.params = []int64{vm.load(vm.instructionPointer + 1)}
+		instruction.paramMode = []ParameterMode{ParameterMode(opcode / 100 % 10)}
+		instruction.length = 2
+	case JumpIfTrue, JumpIfFalse:
+		instruction.params = []int64{vm.load(vm.instructionPointer + 1),
+			vm.load(vm.instructionPointer + 2)}
+		instruction.paramMode = []ParameterMode{ParameterMode(opcode / 100 % 10),
+			ParameterMode(opcode / 1000 % 10)}
+		instruction.length = 3
+	case ProgramStop:
+		vm.stopped = true
+
+	default:
+		panic(fmt.Sprintf("Invalid instruction received: %d", instruction.opcode))
+	}
+
+	return instruction
+}
+
+func (vm *VM) load(address int64) int64 {
+	for address > int64(len(vm.memory)-1) {
+		vm.doubleMemory()
+	}
+	return vm.memory[address]
+}
+
+func (vm *VM) store(address int64, mode ParameterMode, val int64) {
+	for address > int64(len(vm.memory)-1) || address+vm.relativeBase > int64(len(vm.memory)-1) {
+		vm.doubleMemory()
+	}
+
+	if mode == Relative {
+		vm.memory[vm.relativeBase+address] = val
+	} else {
+		vm.memory[address] = val
+	}
+}
+
+func (vm *VM) doubleMemory() {
+	vm.memory = append(vm.memory, make([]int64, len(vm.memory)*2)...)
+}
+
+func (vm *VM) getParamValue(index int64) int64 {
+	value := int64(0)
+	switch vm.currInstruction.paramMode[index] {
+	case Positional:
+		value = vm.load(vm.currInstruction.params[index])
+	case Immediate:
+		value = vm.currInstruction.params[index]
+	case Relative:
+		value = vm.load(vm.relativeBase + vm.currInstruction.params[index])
+	}
+
+	return value
+}
+
+func (vm *VM) executeCurrentInstruction() {
+	switch vm.currInstruction.opcode {
+	case Add:
+		// add the input and store it inside the output
+		vm.store(vm.currInstruction.params[2], vm.currInstruction.paramMode[2], vm.getParamValue(0)+vm.getParamValue(1))
+		// increment the instruction pointer and memory
+		vm.instructionPointer += vm.currInstruction.length
+	case Multiply:
+		// multiply the input and store it inside the output
+		vm.store(vm.currInstruction.params[2], vm.currInstruction.paramMode[2], vm.getParamValue(0)*vm.getParamValue(1))
+		// increment the instruction pointer and memory
+		vm.instructionPointer += vm.currInstruction.length
+	case Input:
+		// store the input of the program
+		vm.store(vm.currInstruction.params[0], vm.currInstruction.paramMode[0], vm.input[0])
+		// increment the instruction pointer and memory
+		vm.instructionPointer += vm.currInstruction.length
+	case Output:
+		// store the instruction output into the VM output and print it to the screen
+		vm.output = vm.getParamValue(0)
+		// increment the instruction pointer and memory
+		vm.instructionPointer += vm.currInstruction.length
+		vm.outputReady = true
+	case JumpIfTrue:
+		if vm.getParamValue(0) != 0 {
+			vm.instructionPointer = vm.getParamValue(1)
+		} else {
+			// increment the instruction pointer and memory
+			vm.instructionPointer += vm.currInstruction.length
+		}
+	case JumpIfFalse:
+		if vm.getParamValue(0) == 0 {
+			vm.instructionPointer = vm.getParamValue(1)
+		} else {
+			// increment the instruction pointer and memory
+			vm.instructionPointer += vm.currInstruction.length
+		}
+	case LessThan:
+		val := int64(0)
+		if vm.getParamValue(0) < vm.getParamValue(1) {
+			val = 1
+		} else {
+			val = 0
+		}
+		vm.store(vm.currInstruction.params[2], vm.currInstruction.paramMode[2], val)
+		// increment the instruction pointer and memory
+		vm.instructionPointer += vm.currInstruction.length
+	case Equals:
+		val := int64(0)
+		if vm.getParamValue(0) == vm.getParamValue(1) {
+			val = 1
+		} else {
+			val = 0
+		}
+		vm.store(vm.currInstruction.params[2], vm.currInstruction.paramMode[2], val)
+		// increment the instruction pointer and memory
+		vm.instructionPointer += vm.currInstruction.length
+	case UpdateRelativeBase:
+		// update the relative base address
+		vm.relativeBase += vm.getParamValue(0)
+		vm.instructionPointer += vm.currInstruction.length
+	case ProgramStop:
+		break
+
+	default:
+		panic("Unknown opcode encountered!")
+	}
+}
+
+func part1(input []int64) int {
+	d := SpringDroid{vm: NewVM()}
+	d.vm.loadProgram(input)
+	script := `NOT C J
+AND D J
+NOT A T
+NOT B J
+AND T J
+NOT C T
+AND D T
+OR T J
+NOT A T
+OR T J
+NOT C T
+AND D T
+OR T J
+WALK
+`
+
+	scriptBytes := []rune(script)
+	return int(d.executeScript(scriptBytes))
+}
+
+func part2(input []int64) int {
+	d := SpringDroid{vm: NewVM()}
+	d.vm.loadProgram(input)
+	script := `NOT T T
+AND A T
+AND B T
+NOT C J
+AND D J
+AND H J
+NOT B T
+AND A T
+AND D T
+OR T J
+NOT A T
+OR T J
+RUN
+`
+	scriptBytes := []rune(script)
+	return int(d.executeScript(scriptBytes))
+}
+
+func (robot *SpringDroid) executeScript(input []rune) int64 {
+	var lastMoments strings.Builder
+	output := int64(0)
+	idx := 0
+
+	for {
+		robot.vm.currInstruction = robot.vm.decodeCurrentInstruction()
+		if robot.vm.currInstruction.opcode == Input {
+			robot.vm.input = []int64{int64(input[idx])}
+			idx++
+			robot.vm.output = 0
+		}
+
+		robot.vm.executeCurrentInstruction()
+
+		if robot.vm.outputReady {
+			robot.vm.outputReady = false
+			if robot.vm.output > 127 {
+				output = robot.vm.output
+				break
+			}
+			lastMoments.WriteRune(rune(robot.vm.output))
+		}
+
+		if robot.vm.hasFinished() {
+			fmt.Println(lastMoments.String())
+			break
+		}
+	}
+	return output
+}
+
+func main() {
+	file, err := os.Open("input/part1.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var inputs []string
+	for scanner.Scan() {
+		inputs = append(inputs, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// we have just one line, with elements separated by commas
+	if len(inputs) != 1 {
+		panic("The input should be only one line long!")
+	}
+
+	var program []int64
+	for _, integer := range strings.Split(inputs[0], ",") {
+		if val, err := strconv.ParseInt(integer, 10, 64); err != nil {
+			panic(err)
+		} else {
+			program = append(program, val)
+		}
+	}
+	// part 1 solution
+	fmt.Println("The result to 1st part is: ", part1(program))
+
+	// part 2 solution
+	fmt.Println("The result to 2nd part is: ", part2(program))
+}
